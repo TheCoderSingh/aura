eb4033ed8475a54f02701f0e29c1ea93b8b258ec
Jaskaran Singh <jaskaran1523@gmail.com>
2020-03-16 02:54:06 -0700

    Create filters

--- src/components/filters/filters.js
+++ src/components/filters/filters.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './filters.scss';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Form, Button } from 'react-bootstrap';
 
 class Filter extends Component {
 	render() {
@@ -8,13 +8,33 @@ class Filter extends Component {
 			<div className="filters-container">
 				<Row>
 					<Col xs={3}>
-
+						<div className="filters-box">
+							<Form>
+								<Form.Group as={Col} controlId="formFilters">
+									{/* <Form.label>Choose a country</Form.Label> */}
+									<Form.Control as="select">
+										<option value="Select Country" selected>Select Country</option>
+									</Form.Control>
+									<Form.Control as="select">
+										<option value="Select Province" selected>Select Province</option>
+									</Form.Control>
+									<Form.Control as="select">
+										<option value="Select City" selected>Select City</option>
+									</Form.Control>
+									<Form.Control type="date">
+									</Form.Control>
+								</Form.Group>
+								<Button variant="primary" type="submit">
+									Check AQI
+  								</Button>
+							</Form>
+						</div>
 					</Col>
 					<Col xs={9}>
 
 					</Col>
 				</Row>
-			</div>
+			</div >
 		);
 	}
 }
