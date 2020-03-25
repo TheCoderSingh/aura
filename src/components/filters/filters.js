e4290807000b48045dbcb219736d5b8239f51844
Jaskaran Singh <jaskaran1523@gmail.com>
2020-03-24 17:48:03 -0700

    Get countries from the API

--- src/components/filters/filters.js
+++ src/components/filters/filters.js
@@ -1,42 +1,25 @@
 import React, { Component } from 'react';
 import './filters.scss';
-import { Row, Col, Form, Button } from 'react-bootstrap';
-import Graph from '../graph/graph';
+import FilterData from './FilterData';
 
 class Filter extends Component {
+	state = {
+		countries: []
+	}
+
+	componentDidMount() {
+		fetch('http://localhost:3002/api/countries')
+			.then(res => res.json())
+			.then((data) => {
+				this.setState({ countries: data })
+			})
+			.catch(console.log())
+	}
+
 	render() {
 		return (
-			<div className="filters-container">
-				<Row>
-					<Col xs={3}>
-						<div className="filters-box">
-							<Form>
-								<Form.Group as={Col} controlId="formFilters">
-									{/* <Form.label>Choose a country</Form.Label> */}
-									<Form.Control as="select">
-										<option value="Select Country" defaultValue>Select Country</option>
-									</Form.Control>
-									<Form.Control as="select">
-										<option value="Select Province" defaultValue>Select Province</option>
-									</Form.Control>
-									<Form.Control as="select">
-										<option value="Select City" defaultValue>Select City</option>
-									</Form.Control>
-									<Form.Control type="date">
-									</Form.Control>
-								</Form.Group>
-								<Button variant="primary" type="submit">
-									Check AQI
-  								</Button>
-							</Form>
-						</div>
-					</Col>
-					<Col xs={9}>
-						<Graph />
-					</Col>
-				</Row>
-			</div >
-		);
+			<FilterData countries={this.state.countries} />
+		)
 	}
 }
 
