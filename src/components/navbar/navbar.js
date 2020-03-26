cf7e9ba6aee8944d2fd097310603322141e7ffe8
Jaskaran Singh <jaskaran1523@gmail.com>
2020-03-26 02:20:34 -0700

    Align navbar menu to right

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,22 +6,29 @@ import { Row, Col } from 'react-bootstrap';
 class NavbarTop extends Component {
 	render() {
 		return (
-			<Row>
-				<Col>
-					<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-						<Navbar.Brand href="/#">Aura</Navbar.Brand>
-						<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-						<Navbar.Collapse id="responsive-navbar-nav">
-							<Nav className="mr-auto">
-								<Nav.Link href="/#">Home</Nav.Link>
-								<Nav.Link href="/#">iAura</Nav.Link>
-								<Nav.Link href="/#">Team</Nav.Link>
-								<Nav.Link href="/#">Contact</Nav.Link>
-							</Nav>
-						</Navbar.Collapse>
-					</Navbar>
-				</Col>
-			</Row>
+			<div className="navbarTop">
+				<Row>
+					<Col xs={9}>
+						<div className="logo">
+							{/* <img src={process.env.PUBLIC_URL + '/images/logo.svg'} /> */}
+						</div>
+					</Col>
+					<Col xs={3}>
+						<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+							{/* <Navbar.Brand href="/#">Aura</Navbar.Brand> */}
+							<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+							<Navbar.Collapse id="responsive-navbar-nav">
+								<Nav className="mr-auto">
+									<Nav.Link href="/#">Home</Nav.Link>
+									<Nav.Link href="/#">iAura</Nav.Link>
+									<Nav.Link href="/#">Team</Nav.Link>
+									<Nav.Link href="/#">Contact</Nav.Link>
+								</Nav>
+							</Navbar.Collapse>
+						</Navbar >
+					</Col>
+				</Row>
+			</div>
 		);
 	}
 }
