683fccb9337e78fdda22e53dee742beb027810fc
Jaskaran Singh <jaskaran1523@gmail.com>
2020-04-01 17:16:08 -0700

    Fix map tile overlay problems

--- src/components/map/GoogleMap.js
+++ src/components/map/GoogleMap.js
@@ -8,36 +8,39 @@ const style = {
 	maxHeight: '340px'
 }
 
-class GoogleMap extends Component {
-	render() {
-		// let map = new window.google.maps.Map(document.getElementById('map'), {
-		// 	center: new window.google.maps.LatLng(51.505, -0.09),
-		// 	mapTypeId: window.google.maps.MapTypeId.ROADMAP,
-		// 	zoom: 11,
-		// });
+let map, waqiMapOverlay;
 
-		var waqiMapOverlay = new window.google.maps.ImageMapType({
+class GoogleMap extends Component {
+	constructor(props) {
+		super(props);
+		console.log(props);
+	}
+	render() {
+		return (
+			<div id="map" className="map-container" >
+			</div>
+		)
+	}
+
+	componentDidMount() {
+		let latlng = new window.google.maps.LatLng(39.305, -76.617);
+		map = new window.google.maps.Map(document.getElementById('map'), {
+			center: latlng,
+			zoom: 5,
+			mapTypeId: window.google.maps.MapTypeId.ROADMAP,
+		});
+
+		waqiMapOverlay = new window.google.maps.ImageMapType({
 			getTileUrl: function (coord, zoom) {
 				return 'https://tiles.aqicn.org/tiles/usepa-aqi/' + zoom + "/" + coord.x + "/" + coord.y + ".png?token=0eb4bf0012292cbdc57e682530d1f6a352894d7e";
 			},
 			name: "Air  Quality",
 		});
 
-		return (
-			<div className="map-container" id="map">
-				<Map
-					google={this.props.google}
-					zoom={11}
-					style={style}
-					center={new window.google.maps.LatLng(51.505, -0.09)}
-					mapTypeId={window.google.maps.MapTypeId.ROADMAP}
-				>
-				</Map>
-			</div>
-		)
+		map.overlayMapTypes.insertAt(0, waqiMapOverlay);
 	}
 }
 
 export default GoogleApiWrapper({
-	apiKey: "AIzaSyCV3jnecKNQQ2dl1ZI7VLNsJAJyiyDRICk"
+	apiKey: "AIzaSyBdw39llza3H7s6eK3R3HDS0ZYYEXgHnGo"
 })(GoogleMap);
