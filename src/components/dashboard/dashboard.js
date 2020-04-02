8c18b4f333baf7e8c9f5cc2d7cb8c051f6254c6d
Jaskaran Singh <jaskaran1523@gmail.com>
2020-04-02 14:22:40 -0700

    Create travel dashboard component

---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './dashboard.scss';
+
+class Dashboard extends Component {
+	render() {
+		return (
+			<Container>
+				<div className="dashboard">
+					<Row>
+						<Col md={4}>
+							<div className="dashboard-box">
+								<div className="dashboard-icon">
+									<img src={process.env.PUBLIC_URL + '/images/precautions.svg'}></img>
+								</div>
+								<h4>Precautions</h4>
+								<p id="dashboard-precautions">
+									<ul>
+										<li>Precaution 1</li>
+										<li>Precaution 2</li>
+										<li>Precaution 3</li>
+									</ul>
+								</p>
+							</div>
+						</Col>
+						<Col md={4}>
+							<div className="dashboard-box">
+								<div className="dashboard-icon">
+									<img src={process.env.PUBLIC_URL + '/images/carryons.svg'}></img>
+								</div>
+								<h4>Carry-Ons</h4>
+								<p id="dashboard-carryons">
+									<ul>
+										<li>Carry-On 1</li>
+										<li>Carry-On 2</li>
+										<li>Carry-On 3</li>
+									</ul>
+								</p>
+							</div>
+						</Col>
+						<Col md={4}>
+							<div className="dashboard-box">
+								<div className="dashboard-icon">
+									<img src={process.env.PUBLIC_URL + '/images/'}></img>
+								</div>
+								<h4>Best Travel Time</h4>
+								<p id="dashboard-traveltime"></p>
+							</div>
+						</Col>
+					</Row>
+				</div>
+			</Container >
+		)
+	}
+}
+
+export default Dashboard;
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,6 +3,7 @@ import Header from '../header/header';
 import Filter from '../filters/Filter';
 import Mailing from '../mailing/mailing';
 import GoogleMap from '../map/GoogleMap';
+import Dashboard from '../dashboard/dashboard';
 
 class Home extends Component {
 	render() {
@@ -10,6 +11,7 @@ class Home extends Component {
 			<div>
 				<Header />
 				<Filter />
+				<Dashboard />
 				<GoogleMap />
 				<Mailing />
 			</div>
