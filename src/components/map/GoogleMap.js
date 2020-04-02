9dda57ce6f685dd606a4f40d74fd17a3f3260771
Jaskaran Singh <jaskaran1523@gmail.com>
2020-04-01 18:55:35 -0700

    Fix map when user denies location

--- src/components/map/GoogleMap.js
+++ src/components/map/GoogleMap.js
@@ -11,10 +11,11 @@ const style = {
 let map, waqiMapOverlay;
 
 class GoogleMap extends Component {
-	constructor(props) {
-		super(props);
-		console.log(props);
-	}
+	state = {
+		latitute: "",
+		longitude: ""
+	};
+
 	render() {
 		return (
 			<div id="map" className="map-container" >
@@ -23,21 +24,70 @@ class GoogleMap extends Component {
 	}
 
 	componentDidMount() {
-		let latlng = new window.google.maps.LatLng(39.305, -76.617);
-		map = new window.google.maps.Map(document.getElementById('map'), {
-			center: latlng,
-			zoom: 5,
-			mapTypeId: window.google.maps.MapTypeId.ROADMAP,
-		});
+		let getUserLocation = () => {
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(getCoordinates, showError);
+			} else {
+				console.log("Geolocation is not supported by this browser.");
+			}
+		}
 
-		waqiMapOverlay = new window.google.maps.ImageMapType({
-			getTileUrl: function (coord, zoom) {
-				return 'https://tiles.aqicn.org/tiles/usepa-aqi/' + zoom + "/" + coord.x + "/" + coord.y + ".png?token=0eb4bf0012292cbdc57e682530d1f6a352894d7e";
-			},
-			name: "Air  Quality",
-		});
+		let getCoordinates = (position) => {
 
-		map.overlayMapTypes.insertAt(0, waqiMapOverlay);
+			this.setState({
+				latitute: position.coords["latitude"],
+				longitude: position.coords["longitude"]
+			});
+
+			createMap();
+		}
+
+		let showError = (error) => {
+			switch (error.code) {
+				case error.PERMISSION_DENIED:
+					console.log("User denied the request for Geolocation.");
+					break;
+				case error.POSITION_UNAVAILABLE:
+					console.log("Location information is unavailable.");
+					break;
+				case error.TIMEOUT:
+					console.log("The request to get user location timed out.");
+					break;
+				case error.UNKNOWN_ERROR:
+					console.log("An unknown error occurred.");
+					break;
+				default:
+					break;
+			}
+
+			this.setState({
+				latitute: "49.282730",
+				longitude: "-123.120735"
+			});
+
+			createMap();
+		}
+
+		getUserLocation(getCoordinates);
+
+		let createMap = () => {
+			let latlng = new window.google.maps.LatLng(this.state.latitute, this.state.longitude);
+
+			map = new window.google.maps.Map(document.getElementById('map'), {
+				center: latlng,
+				zoom: 12,
+				mapTypeId: window.google.maps.MapTypeId.ROADMAP,
+			});
+
+			waqiMapOverlay = new window.google.maps.ImageMapType({
+				getTileUrl: function (coord, zoom) {
+					return 'https://tiles.aqicn.org/tiles/usepa-aqi/' + zoom + "/" + coord.x + "/" + coord.y + ".png?token=0eb4bf0012292cbdc57e682530d1f6a352894d7e";
+				},
+				name: "Air  Quality",
+			});
+
+			map.overlayMapTypes.insertAt(0, waqiMapOverlay);
+		}
 	}
 }
 
