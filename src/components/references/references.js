a4ed7554565278a04db9837e7be2abd40ca890ae
Jaskaran Singh <jaskaran1523@gmail.com>
2020-04-03 01:47:13 -0700

    Update references

--- src/components/references/references.js
+++ src/components/references/references.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 class References extends Component {
-
+	render() {
+		return (
+			<Container className="mt-4 mb-4">
+				<Row>
+					<Col xs={12}>
+						<div className="text-center">
+							<h2>List of References</h2>
+							<ol>
+								<li></li>
+							</ol>
+						</div>
+					</Col>
+				</Row>
+			</Container>
+		)
+	}
 }
 
 export default References;
