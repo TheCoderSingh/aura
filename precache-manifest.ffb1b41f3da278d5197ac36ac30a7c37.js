222d55dfb8aa60f8a6bac09fe8e7570c26adb830
Jaskaran Singh <jaskaran1523@gmail.com>
2020-04-02 19:16:30 -0700

    Updates

---
+++ precache-manifest.ffb1b41f3da278d5197ac36ac30a7c37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd706fa351bfc6d0cf2f4cc15171fda",
+    "url": "/aura/index.html"
+  },
+  {
+    "revision": "eeac0c18235956870451",
+    "url": "/aura/static/css/main.ac16dce0.chunk.css"
+  },
+  {
+    "revision": "84201e877d3f7cc454fa",
+    "url": "/aura/static/js/2.dd3fa81b.chunk.js"
+  },
+  {
+    "revision": "3fac67d07ae546261ad68fa3717f132b",
+    "url": "/aura/static/js/2.dd3fa81b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeac0c18235956870451",
+    "url": "/aura/static/js/main.e7831185.chunk.js"
+  },
+  {
+    "revision": "e4052b7f73deceb9112b",
+    "url": "/aura/static/js/runtime-main.39404d49.js"
+  }
+]);
